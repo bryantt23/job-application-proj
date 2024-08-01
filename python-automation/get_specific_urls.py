b7ff9f6c59e83aae62dfecd4a559c91bd9466148
bryant tunbutr <bryantt23@users.noreply.github.com>
2024-08-01 23:14:27 +0800

    can output to file & also save url needed to apply

--- python-automation/get_specific_urls.py
+++ python-automation/get_specific_urls.py
@@ -6,12 +6,14 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import json
 
-file_path = 'C:/Users/bryan/Desktop/companyDataOutput.json'
+URLS_TO_GET = 3
+file_path_input = 'C:/Users/bryan/Desktop/companyDataOutput.json'
+file_path_output = 'C:/Users/bryan/Desktop/companyDataOutputWithUrls.json'
 try:
-    with open(file_path, 'r') as f:
+    with open(file_path_input, 'r') as f:
         data = json.load(f)
 except FileNotFoundError:
-    print(f"File not found: {file_path}")
+    print(f"File not found: {file_path_input}")
     data = {}
 except json.JSONDecodeError:
     print("Failed to decode JSON from the file.")
@@ -20,8 +22,8 @@ except Exception as e:
     print(f"An error occurred: {e}")
     data = {}
 
-values = list(data.values())[:5]
-urls = list(map(lambda elem: elem["jobs"][0]["url"], values))
+# TODO make it go through the entire dict
+keys = list(data.keys())[:URLS_TO_GET]
 
 # Set up Chrome options
 options = Options()
@@ -37,13 +39,16 @@ service = Service(chrome_driver_path)
 driver = webdriver.Chrome(service=service, options=options)
 
 # Loop through each URL
-for url in urls:
+for key in keys:
     try:
         # Open a new tab and switch to it
         driver.execute_script("window.open('');")
         driver.switch_to.window(driver.window_handles[-1])
 
         # Open the URL
+        value = data[key]
+        job = value["jobs"][0]
+        url = job["url"]
         driver.get(url)
 
         # Adjust the zoom level
@@ -57,7 +62,9 @@ for url in urls:
             )
             application_url = apply_button.get_attribute('href')
             print(f"Application URL: {application_url}")
+            job["job_url"] = application_url
         except Exception as e:
+            job["job_url"] = "Not found"
             print("Apply button not found.")
 
         # Close the current tab
@@ -71,3 +78,11 @@ for url in urls:
 
 # Quit the driver
 driver.quit()
+
+# Write the dictionary to a JSON file
+try:
+    with open(file_path_output, 'w') as f:
+        json.dump(data, f, indent=4)
+        print(f"Data successfully written to {file_path_output}")
+except Exception as e:
+    print(f"An error occurred while writing to the file: {e}")
