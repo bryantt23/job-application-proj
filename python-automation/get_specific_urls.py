16af4691556eecb50b12ebe8300effa40dfd62f4
bryant tunbutr <bryantt23@users.noreply.github.com>
2024-08-04 19:00:12 +0800

    refactor by separating into a function

--- python-automation/get_specific_urls.py
+++ python-automation/get_specific_urls.py
@@ -6,7 +6,28 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import json
 
-URLS_TO_GET = 7
+
+def get_job_url(driver, job):
+    # Wait for the "Apply" button to be present
+    try:
+        driver.get(job['url'])
+        # Adjust the zoom level
+        driver.execute_script("document.body.style.zoom='10%'")
+
+        apply_button = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located(
+                (By.XPATH, "//a[contains(text(), 'Apply')]"))
+        )
+        application_url = apply_button.get_attribute('href')
+        print(f"Application URL: {application_url}")
+        job["job_url"] = application_url
+    except Exception as e:
+        job["job_url"] = "Not found"
+        print("Apply button not found.")
+        print(f"Error: {e}")
+
+
+URLS_TO_GET = 11
 file_path_input = 'C:/Users/bryan/Desktop/companyDataOutput.json'
 file_path_output = 'C:/Users/bryan/Desktop/companyDataOutputWithUrls.json'
 try:
@@ -50,28 +71,10 @@ for key in keys:
         jobs = value["jobs"]
 
         for job in jobs:
-            url = job["url"]
-            driver.get(url)
-
-            # Adjust the zoom level
-            driver.execute_script("document.body.style.zoom='10%'")
-
-            # Wait for the "Apply" button to be present
-            try:
-                apply_button = WebDriverWait(driver, 10).until(
-                    EC.presence_of_element_located(
-                        (By.XPATH, "//a[contains(text(), 'Apply')]"))
-                )
-                application_url = apply_button.get_attribute('href')
-                print(f"Application URL: {application_url}")
-                job["job_url"] = application_url
-            except Exception as e:
-                job["job_url"] = "Not found"
-                print("Apply button not found.")
+            get_job_url(driver, job)
 
         # Close the current tab
         driver.close()
-
         # Switch back to the original tab
         driver.switch_to.window(driver.window_handles[0])
 
